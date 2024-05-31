71eeda42684b038a3e5bd84006331ab62b092f83
Haochen Pan <haochenpan@uchicago.edu>
2024-05-31 17:54:35 -0400

    lambda functions

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,35 +1,40 @@
-"""Lambda function that monitors the AP and reboots the container if needed."""
+"""Lambda function monitor.
+
+a Lambda function that monitors the action provider
+and web services and reboots the containers if needed.
+"""
 
 from __future__ import annotations
 
 from typing import Any
 
 import boto3
-import requests
+import requests  # type:ignore
 
-url = 'https://diaspora-action-provider.ml22sevubfnks.us-east-1.cs.amazonlightsail.com'
-service_name = 'diaspora-action-provider'
+ap_url = 'https://diaspora-action-provider.ml22sevubfnks.us-east-1.cs.amazonlightsail.com'
+ap_service_name = 'diaspora-action-provider'
+web_url = 'https://diaspora-web-service.ml22sevubfnks.us-east-1.cs.amazonlightsail.com'
+web_service_name = 'diaspora-web-service'
 power = 'micro'
 normal_status_code = 200
 
 
-def lambda_handler(event: dict[str, Any] | None, context: Any) -> str | None:
-    """The entry point of the monitor function."""
+def check_service_status(service_name: str, url: str) -> str:
+    """Check the container status and reboot if needed."""
+    client = boto3.client('lightsail')
     action = ''
     try:
-        client = boto3.client('lightsail')
         response = client.get_container_services(serviceName=service_name)
         aws_status = response['containerServices'][0]['state']
-        action = f'Status = {aws_status} '
+        action = f'{service_name} Status = {aws_status} '
     except Exception as e:
-        print('Boto API error, do nothing.')
+        print('Boto API error for', service_name)
         print('ERROR', e)
-        return None
 
     try:
         response = requests.get(url, timeout=7)
         http_code = response.status_code
-        action += f'Code = {http_code} '
+        action += f'{service_name} Code = {http_code} '
     except Exception as e:
         print('ERROR', e)
         http_code = 0
@@ -59,6 +64,14 @@ def lambda_handler(event: dict[str, Any] | None, context: Any) -> str | None:
         action += 'Service in reconfiguration thus no action.'
 
     print(action)
+    return action
+
+
+def lambda_handler(event: dict[str, Any] | None, context: Any) -> str | None:
+    """The entry point of the monitor function."""
+    action = ''
+    action += check_service_status(ap_service_name, ap_url)
+    action += check_service_status(web_service_name, web_url)
     return action
 
 
